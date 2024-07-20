a0173f81994e2f857bcbdcd5146e1935ef7d5750
Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>
2024-07-20 23:24:25 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,15 @@
+import sys
+
+a, b, c = map(int, sys.stdin.readline().split())
+
+def devide(a, b):
+    if b == 1:
+        return a%c
+    else:
+        temp = devide(a, b//2)
+        if b % 2 == 0:
+            return (temp * temp)%c
+        else:
+            return (temp * temp*a)%c
+
+print(devide(a,b))
