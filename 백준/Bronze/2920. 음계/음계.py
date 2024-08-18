3cd32103b9c4718b05ac7897159ed1256d5a9852
Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>
2024-08-18 23:21:27 +0900

    [Bronze II] Title: 음계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split(" ")))
+ascArr = sorted(arr[:])
+desArr = sorted(arr[:], reverse=True)
+
+if arr == ascArr:
+    print("ascending")
+elif arr == desArr:
+    print("descending")
+else:
+    print("mixed")
