ba9ed740a526b3e7c574d7756ad5d46d2c03035e
Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>
2024-09-03 10:36:32 +0900

    [Gold V] Title: 강의실 배정, Time: 4996 ms, Memory: 62724 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,25 @@
+import heapq
+
+q = heapq
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    start, end = map(int, input().split())
+    arr.append((start, end))
+
+result = []
+arr.sort()
+q.heappush(result, arr[0][1])
+
+
+for start, end in arr[1:]:
+    if start < result[0]:
+        q.heappush(result, end)
+    else:
+        q.heappop(result)
+        q.heappush(result, end)
+
+print(len(result))
+
