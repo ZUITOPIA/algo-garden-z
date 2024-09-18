56c2c60e6e812d58c3778fa625582e35c6e46e2d
Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>
2024-09-18 21:13:06 +0900

    [Silver IV] Title: 스택 2, Time: 816 ms, Memory: 102856 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,23 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
+n = int(input())
 arr = []
 
 for _ in range(n):
     command = list(input().split())
-    cmd = int(command[0])
-    
-    if cmd == 1:
+
+    if command[0] == '1':
         arr.append(command[1])
-    elif cmd == 2:
+    elif command[0] == '2':
         if arr:
             print(arr.pop())
         else:
             print(-1)
-    elif cmd == 3:
+    elif command[0] == '3':
         print(len(arr))
-    elif cmd == 4:
+    elif command[0] == '4':
         print(1 if not arr else 0)
-    elif cmd == 5:
+    elif command[0] == '5':
         print(arr[-1] if arr else -1)
