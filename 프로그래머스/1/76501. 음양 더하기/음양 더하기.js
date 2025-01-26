a3a3c6d72b255c055642672b03c11ea5019771ad
Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>
2025-01-26 19:42:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+    let result = 0
+    absolutes.forEach((item, index) => {
+        if(signs[index]) result += item
+        else result -= item
+    })
+    return result
+}
