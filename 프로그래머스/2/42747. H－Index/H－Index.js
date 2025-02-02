9c70b006a992eef8eb27d375674f0883eeaac1f1
Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>
2025-02-02 11:49:58 +0900

    [level 2] Title: H-Index, Time: 0.67 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,11 +1,11 @@
 function solution(citations) {
     let answer = 0
     
-    let sorted_citations = citations.sort((a,b) => b-a) 
+    citations.sort((a,b) => b-a) 
     
-    for(let i=0; i<sorted_citations.length; i++){
+    for(let i=0; i<citations.length; i++){
         let h = i+1
-        if(sorted_citations[i] >= h) answer=h
+        if(citations[i] >= h) answer=h
         else break
     }
     
